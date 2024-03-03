177d6cb5ffa093552fd05502052f7c6a9ca9a8c4
klinton.daltio@gmail.com <@daltiosk77>
2024-03-03 15:06:31 -0300

    Video 6: Component Scan. Escaneia vários pacotes para carregar os beans

--- src/main/java/test/outside/OutsideController.java
+++ src/main/java/test/outside/OutsideController.java
@@ -1,16 +1,14 @@
-package controller;
+package test.outside;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("greatings")
-public class HelloController {
-
-    @GetMapping("hi")
-     public String hi(){
-         return "hello";
-     }
-
+@RequestMapping("test")
+public class OutsideController {
+    @GetMapping("outside")
+    public String outsideTest(){
+        return "outside test";
+    }
 }
