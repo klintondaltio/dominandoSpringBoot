4a2793f4d434c7e85f465e076cbeb51ce36df8ca
klinton.daltio@gmail.com <@daltiosk77>
2024-03-06 21:31:53 -0300

    Video 14: ResponseEntity

--- src/main/java/academy/devdojo/controller/ProducerController.java
+++ src/main/java/academy/devdojo/controller/ProducerController.java
@@ -3,7 +3,9 @@ package academy.devdojo.controller;
 import academy.devdojo.domain.Anime;
 import academy.devdojo.domain.Producer;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProducerController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE, headers = "x-api-version=v1")
-    public Producer save(@RequestBody Producer producer){
+    public ResponseEntity<Producer> save(@RequestBody Producer producer){
         log.info("Request received to list all animes, param name: '{}'", producer.getName());
         producer.setId(ThreadLocalRandom.current().nextLong(100_000));
         Producer.getProducer().add(producer);
-        return producer;
+        return ResponseEntity.status(HttpStatus.CREATED).body(producer);
     }
 }
